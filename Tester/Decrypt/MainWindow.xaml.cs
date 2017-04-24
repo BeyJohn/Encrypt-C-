211dd019864aac3a5dcedb47aa49aac38eb0c7f5
BeyJohn <beyjohn1@gmail.com>
2017-04-24 13:28:13 -0500

    Finished most of everyhting.

--- Tester/Decrypt/MainWindow.xaml.cs
+++ Tester/Decrypt/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Encrypt
+namespace Decrypt
 {
     
     public partial class MainWindow : Window
@@ -29,18 +29,18 @@ namespace Encrypt
             done = false;
             files = new List<string>();
             GetFiles();
-            foreach(string f in files)
+            foreach (string f in files)
                 cbItems.Items.Add(f);
         }
 
         private void GetFiles()
         {
-            if(File.GetAttributes(AppDomain.CurrentDomain.BaseDirectory).ToString() == "Directory")
+            if (File.GetAttributes(AppDomain.CurrentDomain.BaseDirectory).ToString() == "Directory")
             {
                 string[] f = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory);
-                foreach(string h in f)
+                foreach (string h in f)
                 {
-                    if(File.GetAttributes(h).ToString() != "Directory")
+                    if (File.GetAttributes(h).ToString() != "Directory")
                     {
                         //tbPrompt.Text += "\n" + h.Substring(AppDomain.CurrentDomain.BaseDirectory.ToString().Length) + ":" + File.GetAttributes(h).ToString();
                         files.Add(h.Substring(AppDomain.CurrentDomain.BaseDirectory.ToString().Length));
@@ -51,7 +51,7 @@ namespace Encrypt
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if(tbNewFile.Text != "" && cbItems.Text != "" && !done)
+            if (tbNewFile.Text != "" && cbItems.Text != "" && !done)
             {
                 Start(AppDomain.CurrentDomain.BaseDirectory + cbItems.Text, AppDomain.CurrentDomain.BaseDirectory + tbNewFile.Text);
                 done = true;
@@ -61,66 +61,41 @@ namespace Encrypt
         private void Start(string inFile, string outFile)
         {
 
-            File.WriteAllLines(outFile, Enc(File.ReadAllLines(inFile)));
+            File.WriteAllLines(outFile, Dec(File.ReadAllLines(inFile)));
             tbPrompt.Text = File.ReadAllText(outFile);
         }
-
-        private string[] Enc(string[] g)
+        
+        private string[] Dec(string[] g)
         {
             for (int o = 0; o < g.Length; o++)
             {
-                g[o] = Enc(g[o]);
+                g[o] = Dec(g[o]);
             }
-                
+
             return g;
         }
-
-        private string Enc(string g)
+        
+        private string Dec(string g)
         {
             string h = "";
-            for(int o = 0; o < g.Length; o++)
+            g = g.Replace('G', '0').Replace('C', '1').Replace('A', '2').Replace('T', '3'); 
+            while(g.Length > 0)
             {
-                char[] c = g.ToCharArray();
-                string temp = DecimalToBaseFour(c[o]);
-                while (temp.Length < 4)
-                    temp = "0" + temp;
-                h += temp;
-                h = h.Replace('0', 'G').Replace('1', 'C').Replace('2', 'A').Replace('3', 'T');
+                h += (char)BaseFourToDecimal(g.Substring(0, 4));
+                g = g.Substring(4);
             }
             return h;
         }
         
-        public string DecimalToBaseFour(int decimalNumber)
+        public int BaseFourToDecimal(string baseFour)
         {
-            const int BitsInInt = 32;
-            const string Digits = "0123";
-
-            int radix = 4;
-
-            if (radix < 2 || radix > Digits.Length)
-                throw new ArgumentException("The radix must be >= 2 and <= " + Digits.Length.ToString());
-
-            if (decimalNumber == 0)
-                return "0";
-
-            int index = BitsInInt - 1;
-            long currentNumber = Math.Abs(decimalNumber);
-            char[] charArray = new char[BitsInInt];
-
-            while (currentNumber != 0)
+            int y = 0;
+            char[] c = baseFour.ToCharArray();
+            for(int o = 3; o >= 0; o--)
             {
-                int remainder = (int)(currentNumber % radix);
-                charArray[index--] = Digits[remainder];
-                currentNumber = currentNumber / radix;
+                y += (c[o] - 48) * (int)Math.Pow(4, 3 - o);
             }
-
-            string result = new String(charArray, index + 1, BitsInInt - index - 1);
-            if (decimalNumber < 0)
-            {
-                result = "-" + result;
-            }
-
-            return result;
+            return y;
         }
 
     }
