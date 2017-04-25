c6ed7949de4a2e07393c41e8c9cdc4237773afd1
John <Beyjohn1@gmail.com>
2017-04-24 23:32:50 -0500

    Improved and fixed sln file

--- Tester/Decrypt/MainWindow.xaml.cs
+++ Tester/Decrypt/MainWindow.xaml.cs
@@ -5,24 +5,22 @@ using System.Windows;
 
 namespace Decrypt
 {
-    
-    public partial class MainWindow : Window
-    {
-
-        private static List<string> files;
+	public partial class MainWindow : Window
+	{
+		private static List<string> files;
 		private string loc;
 
 		public MainWindow()
-        {
-            InitializeComponent();
+		{
+			InitializeComponent();
 
 			loc = AppDomain.CurrentDomain.BaseDirectory;
 			files = new List<string>();
 
-            GetFiles();
-            foreach (string f in files)
-                cbItems.Items.Add(f);
-        }
+			GetFiles();
+			foreach (string f in files)
+				cbItems.Items.Add(f);
+		}
 
 		private void GetFiles()
 		{
@@ -37,53 +35,50 @@ namespace Decrypt
 		}
 
 		private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (tbNewFile.Text != "" && cbItems.Text != "")
-            {
-                Start(loc + cbItems.Text, loc + tbNewFile.Text);
-            }
-        }
+		{
+			if (tbNewFile.Text != "" && cbItems.Text != "")
+			{
+				Start(loc + cbItems.Text, loc + tbNewFile.Text);
+			}
+		}
 
-        private void Start(string inFile, string outFile)
-        {
+		private void Start(string inFile, string outFile)
+		{
+			File.WriteAllLines(outFile, Dec(File.ReadAllLines(inFile)));
+			tbPrompt.Text = File.ReadAllText(outFile);
+		}
 
-            File.WriteAllLines(outFile, Dec(File.ReadAllLines(inFile)));
-            tbPrompt.Text = File.ReadAllText(outFile);
-        }
-        
-        private string[] Dec(string[] g)
-        {
-            for (int o = 0; o < g.Length; o++)
-            {
-                g[o] = Dec(g[o]);
-            }
+		private string[] Dec(string[] g)
+		{
+			for (int o = 0; o < g.Length; o++)
+			{
+				g[o] = Dec(g[o]);
+			}
 
-            return g;
-        }
-        
-        private string Dec(string g)
-        {
-            string h = "";
-            g = g.Replace('G', '0').Replace('C', '1').Replace('A', '2').Replace('T', '3'); 
-            while(g.Length > 0)
-            {
-                h += (char)BaseFourToDecimal(g.Substring(0, 4));
-                g = g.Substring(4);
-            }
-            return h;
-        }
-        
-        public int BaseFourToDecimal(string baseFour)
-        {
-            int y = 0;
-            char[] c = baseFour.ToCharArray();
-            for(int o = 3; o >= 0; o--)
-            {
-                y += (c[o] - 48) * (int)Math.Pow(4, 3 - o);
-            }
-            return y;
-        }
+			return g;
+		}
 
-    }
+		private string Dec(string g)
+		{
+			string h = "";
+			g = g.Replace('G', '0').Replace('C', '1').Replace('A', '2').Replace('T', '3');
+			while (g.Length > 0)
+			{
+				h += (char)BaseFourToDecimal(g.Substring(0, 4));
+				g = g.Substring(4);
+			}
+			return h;
+		}
 
+		public int BaseFourToDecimal(string baseFour)
+		{
+			int y = 0;
+			char[] c = baseFour.ToCharArray();
+			for (int o = 3; o >= 0; o--)
+			{
+				y += (c[o] - 48) * (int)Math.Pow(4, 3 - o);
+			}
+			return y;
+		}
+	}
 }
--- Tester/Encrypt/MainWindow.xaml.cs
+++ Tester/Encrypt/MainWindow.xaml.cs
@@ -5,104 +5,84 @@ using System.Windows;
 
 namespace Encrypt
 {
-    
-    public partial class MainWindow : Window
-    {
-
-        private static List<string> files;
+	public partial class MainWindow : Window
+	{
+		private static List<string> files;
 		private string loc;
 
-        public MainWindow()
-        {
-            InitializeComponent();
+		public MainWindow()
+		{
+			InitializeComponent();
 
 			loc = AppDomain.CurrentDomain.BaseDirectory;
 			files = new List<string>();
 
 			GetFiles();
 			foreach (string f in files)
-                cbItems.Items.Add(f);
-        }
+				cbItems.Items.Add(f);
+		}
 
-        private void GetFiles()
-        {
-            DirectoryInfo d = new DirectoryInfo(loc);
-            foreach (FileInfo h in d.GetFiles())
-            {
-                if(File.GetAttributes(h.Name).ToString() != "Directory")
-                {
-                    files.Add(h.Name);
-                }
-            }
-        }
+		private void GetFiles()
+		{
+			DirectoryInfo d = new DirectoryInfo(loc);
+			foreach (FileInfo h in d.GetFiles())
+			{
+				if (File.GetAttributes(h.Name).ToString() != "Directory")
+				{
+					files.Add(h.Name);
+				}
+			}
+		}
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if(tbNewFile.Text != "" && cbItems.Text != "")
-            {
-                Start(loc + cbItems.Text, loc + tbNewFile.Text);
-            }
-        }
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			if (tbNewFile.Text != "" && cbItems.Text != "")
+			{
+				Start(loc + cbItems.Text, loc + tbNewFile.Text);
+			}
+		}
 
-        private void Start(string inFile, string outFile)
-        {
+		private void Start(string inFile, string outFile)
+		{
+			File.WriteAllLines(outFile, Enc(File.ReadAllLines(inFile)));
+			tbPrompt.Text = File.ReadAllText(outFile);
+		}
 
-            File.WriteAllLines(outFile, Enc(File.ReadAllLines(inFile)));
-            tbPrompt.Text = File.ReadAllText(outFile);
-        }
+		private string[] Enc(string[] g)
+		{
+			for (int o = 0; o < g.Length; o++)
+			{
+				g[o] = Enc(g[o]);
+			}
 
-        private string[] Enc(string[] g)
-        {
-            for (int o = 0; o < g.Length; o++)
-            {
-                g[o] = Enc(g[o]);
-            }
-                
-            return g;
-        }
+			return g;
+		}
 
-        private string Enc(string g)
-        {
-            string h = "";
-            for(int o = 0; o < g.Length; o++)
-            {
-                char[] c = g.ToCharArray();
-                string temp = DecimalToBaseFour(c[o]);
-                while (temp.Length < 4)
-                    temp = "0" + temp;
-                h += temp;
-                h = h.Replace('0', 'G').Replace('1', 'C').Replace('2', 'A').Replace('3', 'T');
-            }
-            return h;
-        }
-        
-        public string DecimalToBaseFour(int decimalNumber)
-        {
-            const int BitsInInt = 32;
-            const string Digits = "0123";
+		private string Enc(string g)
+		{
+			string h = "";
+			for (int o = 0; o < g.Length; o++)
+			{
+				char[] c = g.ToCharArray();
+				h += DecimalToBaseFour(c[o]);
+				h = h.Replace('0', 'G').Replace('1', 'C').Replace('2', 'A').Replace('3', 'T');
+			}
+			return h;
+		}
 
-            int radix = 4;
-			
+		public string DecimalToBaseFour(char decimalNumber)
+		{
+			int index = 3;
+			long currentNumber = Math.Abs(decimalNumber);
+			char[] charArray = new char[4];
 
-            if (decimalNumber == 0)
-                return "0";
+			while (index >= 0)
+			{
+				charArray[index--] = (char)(currentNumber % 4);
+				currentNumber = currentNumber / 4;
+			}
 
-            int index = BitsInInt - 1;
-            long currentNumber = Math.Abs(decimalNumber);
-            char[] charArray = new char[BitsInInt];
-
-            while (currentNumber != 0)
-            {
-                int remainder = (int)(currentNumber % radix);
-                charArray[index--] = Digits[remainder];
-                currentNumber = currentNumber / radix;
-            }
-
-            string result = new String(charArray, index + 1, BitsInInt - index - 1);
-
-            return result;
-        }
-
-    }
-
+			return new String(charArray);
+		}
+	}
 }
